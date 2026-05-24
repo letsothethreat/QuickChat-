c5ce3bb8e55e8b34ab765a04b74ed915a12888b6
Student <Student@LabVM2047644.wtdmxwxkjv5ulcm4xan4eivoub.jnbx.internal.cloudapp.net>
2026-05-24 20:48:18 +0200

    first commit[edited]

--- src/main/java/com/mycompany/quickchat/QuickChat.java
+++ src/main/java/com/mycompany/quickchat/QuickChat.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.quickchat;
-
+import java.util.Scanner;
 /**
  *
  * @author Student
@@ -11,6 +11,72 @@ package com.mycompany.quickchat;
 public class QuickChat {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Login login = new Login();
+        Scanner input = new Scanner(System.in);
+        
+        
+        System.out.println("*****Welcome to QuickChat*****");
+        System.out.println("1. Register");
+        System.out.println("2. Login");
+        System.out.println("Choose an option(1 or 2):");
+        
+        boolean loggedIn = false;
+        if (choice.equals("1")) {
+            login.registerUser();
+        } else if (choice.equals("2")) {
+            loggedIn = login.loginUser();
+        }else {
+            System.out.println("Invalid option, please restart and choose 1 or 2.");
+            return;
+                    
+        }
+        
+        if (!loggedIn) {
+            System.out.println("You must be logged in to send messages!");
+            return;
+            
+        }
+        System.out.println("******\nWelcome to QuickChat.*****");
+        System.out.println("How many message would you like to send?");
+        int numMessages = Integer.parseInt(input.nextLine().trim());
+        
+        int count = 0;
+        while (true) {
+            System.out.println("\n1) Send Messages");
+            System.out.println("2) Show recently sent messages");
+            System.out.println("3) Quit");
+            System.out.println("Choose an option: ");
+            String menuChoice = input.nextLine().trim();
+            
+            if (menuChoice.equals("1")) {
+                if (count >= numMessages) {
+                    System.out.println("You have reached your message limit!");
+                    continue;
+                }
+                
+                System.out.println("Enter recipient (+27. . .): ");
+                String recipient = input.nextLine().trim();
+                
+                System.out.println("Enter message: ");
+                String messageText = input.nextLine().trim();
+                
+                //Check message length
+                String lengthCheck = Messages.checkMessageLength(messageText);
+                System.out.println(lengthCheck);
+                if (messageText.length() > 250) continue;
+                
+                Messages msg = new Messages(recipient, messageText);
+                
+                //Check recipient
+                System.out.println(msg.checkRecipientCell());
+                
+                System.out.println("\n1) Send Message");
+                System.out.println("2) Disregard Message");
+                System.out.println("3) Store Message");
+                System.out.println("Choose an option: ");
+                
+            }
+        }
     }
 }
